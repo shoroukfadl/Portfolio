f02cd19a1d51df6e32c623be7c662b532e7665ca
shorouk <shoroukfadl975@gmail.com>
2026-06-09 23:29:38 +0300

    CI/CD TEST

--- lib/Core/Api/api_service.dart
+++ lib/Core/Api/api_service.dart
@@ -1,4 +1,2 @@
- // const key = String.fromEnvironment('https://zljhjxsdlidmfpfwmvpa.supabase.co');
- const key = 'https://zljhjxsdlidmfpfwmvpa.supabase.co';
- const anKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsamhqeHNkbGlkbWZwZndtdnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3Njk3NzQsImV4cCI6MjA5MjM0NTc3NH0.Vv0wRPesQi7BG-iw5SECV_yUmHeZ8IYqlGJreN0thu4';
-// const anKey = String.fromEnvironment('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsamhqeHNkbGlkbWZwZndtdnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3Njk3NzQsImV4cCI6MjA5MjM0NTc3NH0.Vv0wRPesQi7BG-iw5SECV_yUmHeZ8IYqlGJreN0thu4');const anKey = String.fromEnvironment('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsamhqeHNkbGlkbWZwZndtdnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3Njk3NzQsImV4cCI6MjA5MjM0NTc3NH0.Vv0wRPesQi7BG-iw5SECV_yUmHeZ8IYqlGJreN0thu4');
+const key = String.fromEnvironment('SUPABASE_URL');
+const anKey = String.fromEnvironment('SUPABASE_ANON_KEY');
