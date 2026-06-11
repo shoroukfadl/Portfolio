3ffe32e378e0d9fe821cdf4749dbbcd549173f5e
shorouk <shoroukfadl975@gmail.com>
2026-06-11 18:44:31 +0300

    Deploy To GitHub

--- lib/Widgets/Portfilio/profile_image.dart
+++ lib/Widgets/Portfilio/profile_image.dart
@@ -1,28 +1,81 @@
-
 import 'package:flutter/material.dart';
 import 'package:portfolio/Utilities/extensions.dart';
-
 import '../rounded_image_widget.dart';
 
 class ProfileImage extends StatefulWidget {
   final String image;
-  final double width , height;
-  const ProfileImage({super.key, required this.image,  this.width =180,  this.height = 180});
+  final double width, height;
+
+  const ProfileImage({
+    super.key,
+    required this.image,
+    this.width = 180,
+    this.height = 180,
+  });
 
   @override
   State<ProfileImage> createState() => _ProfileImageState();
 }
 
 class _ProfileImageState extends State<ProfileImage> {
+
+  bool _isHovered = false;
+
+
+
+  void _onHoverChanged(bool value) {
+    setState(() => _isHovered = value);
+
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final colors = context.colors;
-    return   RoundedImage(
-      imagePath:widget.image,
+
+    final glow = AnimatedContainer(
       height: widget.height,
       width: widget.width,
-      padding: 0,
-      borderColor: colors.accent,
+      duration: const Duration(microseconds: 500),
+      decoration: BoxDecoration(
+        color: colors.secondary.withValues(alpha: 0.35),
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: colors.secondary.withValues(alpha: 0.2),
+            blurRadius:20,
+            spreadRadius:  12 ,
+          ),
+          if(_isHovered)
+          BoxShadow(
+            color:   colors.accent.withValues(alpha: 0.2),
+            blurRadius:20,
+            spreadRadius:  12 ,
+          ),
+        ],
+      ),
+    );
+
+    return MouseRegion(
+      onEnter: (_) => _onHoverChanged(true),
+      onExit: (_) => _onHoverChanged(false),
+      child: Stack(
+        children: [
+          AnimatedScale(
+            duration: const Duration(milliseconds: 250),
+            scale:  _isHovered ? 1.07: 1,
+            child: glow,
+          ),
+      RoundedImage(
+        imagePath:widget.image,
+        height: widget.height,
+        width: widget.width,
+        padding: 0,
+        borderColor: colors.accent,
+      )
+        ],
+      )
     );
   }
 }
